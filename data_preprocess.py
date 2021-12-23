e9cd3d7368a9b4d57821e569af7ad01e09269d2e
WKATZZL <TJzengle@163.com>
2021-12-23 22:06:30 +0800

    preprocess stage 2

--- data_preprocess.py
+++ data_preprocess.py
@@ -2,8 +2,11 @@ import pandas as pd
 import os
 
 
-def read_file(file_name):
-    file = pd.read_csv(file_name, header=None, sep=',')
+def read_file(file_name, is_skip_header=True):
+    if is_skip_header:
+        file = pd.read_csv(file_name, header=None, sep=',')
+    else:
+        file = pd.read_csv(file_name, sep=',')
     return file
 
 
@@ -18,6 +21,32 @@ def delete_empty_file(root='origin'):
                 file = read_file(file_path)
                 if len(file) > 1:
                     f.write(file_path + '\n')
+
+
+def delete_lack_frame_file(rate=0.08):
+    f = open("non_empty_file_list.txt", "r")
+    lines = f.readlines()
+    f.close()
+    count = 0
+    with open("non_lack_frame_file_list.txt", "w") as f:
+        for line in lines:
+            line = line.strip()
+            video_file = read_file(line.strip(), False)
+            max_frame = max(video_file["frame"].drop_duplicates().tolist())
+            if (len(video_file) - 1) / max_frame / 68 > 1 - rate:
+                f.write(line + '\n')
+            else:
+                count += 1
+    print(count / len(lines))
+
+
+def interpolate(method="linear"):
+    f = open("non_lack_frame_file_list.txt", "r")
+    lines = f.readlines()
+    f.close()
+    count = 0
+    with open("interpolate_frame_file_list.txt", "w") as f:
+        pass
 
 
 
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from data_preprocess import *
 
 if __name__ == '__main__':
-    print(read_file("./origin/level_0/level_0 (10).csv"))
     delete_empty_file('./origin')
+    delete_lack_frame_file()
+    interpolate()
