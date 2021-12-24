1522c77cdbaa09ae23bb6d9d81108eceb7b76237
WKATZZL <TJzengle@163.com>
2021-12-24 14:17:02 +0800

    preprocess stage 2

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import os
 
@@ -33,7 +34,7 @@ def delete_lack_frame_file(rate=0.08):
             line = line.strip()
             video_file = read_file(line.strip(), False)
             max_frame = max(video_file["frame"].drop_duplicates().tolist())
-            if (len(video_file) - 1) / max_frame / 68 > 1 - rate:
+            if len(video_file) / max_frame / 68 > 1 - rate:
                 f.write(line + '\n')
             else:
                 count += 1
@@ -46,7 +47,35 @@ def interpolate(method="linear"):
     f.close()
     count = 0
     with open("interpolate_frame_file_list.txt", "w") as f:
-        pass
+        for line in lines:
+            line = line.strip()
+            video_file = read_file(line.strip(), False)
+            video_file.columns = ["No", "frame", "x", "y"]
+            line = line.replace("origin", "data")
+            frames = video_file["frame"].drop_duplicates().tolist()
+            max_frame = max(frames)
+            if len(video_file) == max_frame * 68:
+                video_file.to_csv(line, index=False)
+            else:
+                print(line)
+                print(frames)
+                j = 0
+                for i in range(1, max_frame + 1):
+                    if frames[j] != i:
+                        df1 = video_file.iloc[:(i-1)*68, :]
+                        df2 = video_file.iloc[(i-1)*68:, :]
+                        df_add = np.full((68, 4), np.nan)
+                        df_add[:, 1] = i
+                        df_add[:, 0] = range(68)
+                        df_add = pd.DataFrame(df_add, columns=["No", "frame", "x", "y"])
+                        video_file = pd.concat((df1, df_add, df2), ignore_index=True)
+                    else:
+                        j += 1
+                for i in range(68):
+                    video_file[i:-1:68] = video_file[i:-1:68].interpolate(method=method)
+                video_file.to_csv(line, index=False)
+
+    print(count / len(lines))
 
 
 
