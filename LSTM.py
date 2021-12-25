4f5db1302e419380916e8235dae7f0559eaa9e36
WKATZZL <TJzengle@163.com>
2021-12-25 15:38:48 +0800

    dataset

---
+++ LSTM.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+class LSTM(nn.Module):
+    def __init__(self, input_size=1, hidden_layer_size=100, num_layers=10, output_size=3):
+        super(LSTM, self).__init__()
+        self.hidden_layer_size = hidden_layer_size
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_layer_size,
+                            num_layers=num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_layer_size, output_size)
+
+    def forward(self, x):
+        lstm_out, _ = self.lstm(x)
+        output = self.fc(lstm_out[:, 1, :])
+        return output
+
